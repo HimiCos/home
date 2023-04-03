c073fa0842ffa4ea7e8981769f8d8ae93026f8fe
HimiCos <92566148+HimiCos@users.noreply.github.com>
2023-04-03 21:02:20 +0800

    Update index.vue

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -38,7 +38,7 @@ import {
   Compass,
   Book,
   Flask,
-  Message-bot,
+  Chair,
 } from "@vicons/fa";
 
 // 网站链接数据
@@ -65,7 +65,7 @@ let linksData = [
     link: "https://nav.himicos.cc/",
   },
   {
-    icon: Message-bot,
+    icon: Chair,
     name: "網址集",
     link: "https://openai.himicos.cc/",
   },

8e90ff25a6e1513716b2b331feb53fb2051c09ae
HimiCos <92566148+HimiCos@users.noreply.github.com>
2023-04-11 14:51:14 +0800

    Update index.vue

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -14,7 +14,7 @@
           {{ config.author }}
         </a></span
         >
-        >&nbsp;&amp;
+        &nbsp;&amp;
         <!-- 站点备案 -->
         <a href="https://beian.miit.gov.cn" target="_blank"
           >鲁ICP备2023007969号</a
